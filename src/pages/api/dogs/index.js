c8d08357369910365d1a587d5ba4e7fcb0ed7745
Trisha S Shetty <shettytrisha197@gmail.com>
2024-11-21 09:40:04 +0530

    Added dogs in Dogs page

--- src/pages/api/dogs/index.js
+++ src/pages/api/dogs/index.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 					name: true,
 					breed: true,
 					age: true,
-					size: true,
+					description: true,
 					imageUrl: true,
 				},
 			});
--- src/pages/dogs/index.jsx
+++ src/pages/dogs/index.jsx
@@ -27,12 +27,12 @@ const DogsPage = () => {
 	return (
 		<main className={`w-full h-screen`}>
 			<MainNav className="w-full px-8 py-3 border-b border-b-border absolute top-0" />
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-20 h-full px-4">
-				{dogs.length === 0 && <p>NO DOGS FOUND</p>}
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 pt-20 px-4">
+				{dogs.length === 0 && <p className="col-span-full text-center">NO DOGS FOUND</p>}
 				{dogs.map((dog) => (
 					<div
 						key={dog.id}
-						className="card bg-card border border-border w-fit h-fit shadow-lg p-4 rounded-lg"
+						className="card bg-card border border-border shadow-lg p-4 rounded-lg flex flex-col items-center"
 					>
 						<Image
 							src={dog.imageUrl || "/placeholder-image.jpg"}
@@ -41,21 +41,22 @@ const DogsPage = () => {
 							height={500}
 							className="w-full h-48 object-cover rounded-lg mb-4"
 						/>
-						<h2 className="text-xl font-semibold text-card-foreground">
+						<h2 className="text-xl font-semibold text-card-foreground text-center">
 							{dog.name}
 						</h2>
-						<p className="text-card-foreground/50">Breed: {dog.breed}</p>
-						<p className="text-card-foreground/50">Age: {dog.age} years</p>
-						<p className="text-card-foreground/50">Size: {dog.size}</p>
+						<p className="text-card-foreground/50 text-center">Breed: {dog.breed}</p>
+						<p className="text-card-foreground/50 text-center">Age: {dog.age} years</p>
+						<p className="text-card-foreground/50 text-center">Description: {dog.description}</p>
 						<Link
 							href={`/dogs/${dog.id}`}
-							className="text-primary hover:underline mt-2 block"
+							className="text-primary hover:underline mt-2 block text-center"
 						>
 							View More
 						</Link>
 					</div>
 				))}
 			</div>
+
 		</main>
 	);
 };
