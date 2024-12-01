dcc475c1cdc0e10a33eb68e567d299fdb7228f59
Amrith R Naik <amrithrnaik@gmail.com>
2024-12-01 14:21:26 +0530

    [style] added icons to the navbar menu

--- src/components/main-nav.jsx
+++ src/components/main-nav.jsx
@@ -10,6 +10,7 @@ import { ThemeToggle } from "./theme-toggle";
 import { Button } from "./ui/button";
 import { signOut, useSession } from "next-auth/react";
 import { useState } from "react";
+import { Heart, Home, PawPrint } from "lucide-react";
 
 export function MainNav({ className }) {
 	const [isLoading, setIsLoading] = useState(false);
@@ -33,7 +34,7 @@ export function MainNav({ className }) {
 				className
 			)}
 		>
-			<div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-1">
+			<div className="max-w-[80%] mx-auto flex justify-between items-center px-6 py-1">
 				{/* Logo */}
 				<Link href="/" className="flex items-center space-x-2">
 					<Icons.logo className="h-10 w-10" />
@@ -42,68 +43,84 @@ export function MainNav({ className }) {
 					</span>
 				</Link>
 
-				{/* Navigation */}
-				<nav className="flex items-center gap-6 text-sm xl:gap-8">
-					<Link
-						href="/dogs"
-						className={cn(
-							"transition-colors hover:text-primary",
+				{/* Navigation menu*/}
+				<nav className="flex items-center gap-8 text-sm xl:gap-8">
+					{/* Dogs page button */}
+					<Button
+						variant="ghost"
+						size="sm"
+						className={
 							pathname === "/dogs" ? "text-primary" : "text-muted-foreground"
-						)}
+						}
+						onClick={() => router.push("/dogs")}
 					>
-						Dogs
-					</Link>
-					<Link
-						href="/dogBreed"
-						className={cn(
-							"transition-colors hover:text-primary",
+						<PawPrint size={16} />
+						<p>Dogs</p>
+					</Button>
+
+					{/* TODO : Remove the breeds page and shift all the necessary options to dog page itself */}
+					<Button
+						variant="ghost"
+						size="sm"
+						className={
 							pathname === "/dogBreed"
 								? "text-primary"
 								: "text-muted-foreground"
-						)}
+						}
+						onClick={() => router.push("/dogBreed")}
 					>
-						Breeds
-					</Link>
+						<p>Breeds</p>
+					</Button>
+
+					{/* Shelters Page button */}
+					<Button
+						variant="ghost"
+						size="sm"
+						className={
+							pathname === "/shelters"
+								? "text-primary"
+								: "text-muted-foreground"
+						}
+						onClick={() => router.push("/shelters")}
+					>
+						<Home size={16} />
+						<p>Shelters</p>
+					</Button>
+
+					{/* Favorites Page Button */}
+					{session?.user?.role === "SHELTER_STAFF" && (
+						<Button
+							variant="ghost"
+							size="sm"
+							className={
+								pathname === "/favorite"
+									? "text-primary"
+									: "text-muted-foreground"
+							}
+							onClick={() => router.push("/favorite")}
+						>
+							<Heart size={16} />
+							<p>My Favorites</p>
+						</Button>
+					)}
+
+					{/* Shelter Dashboard Page Button */}
 					{session?.user?.role === "SHELTER_STAFF" && (
 						<Link
 							href="/shelterDashboard"
 							className={cn(
-								"transition-colors hover:text-primary",
+								"transition-colors hover:text-primary hover:border-primary border border-secondary rounded-full px-3 py-1",
 								pathname?.startsWith("/shelterDashboard")
 									? "text-primary"
-									: "text-muted-foreground"
+									: "text-accent"
 							)}
 						>
 							Dashboard
 						</Link>
 					)}
-					<Link
-						href="/shelters"
-						className={cn(
-							"transition-colors hover:text-primary",
-							pathname?.startsWith("/shelters")
-								? "text-primary"
-								: "text-muted-foreground"
-						)}
-					>
-						Shelters
-					</Link>
-					{session?.user?.role === "SHELTER_STAFF" && (
-						<Link
-							href="/favorite"
-							className={cn(
-								"transition-colors hover:text-primary",
-								pathname?.startsWith("/favorite")
-									? "text-primary"
-									: "text-muted-foreground"
-						)}
-					>
-						Favorites
-					</Link>
-				)}
 				</nav>
 
-				{/* Theme toggle button and sign-in/logout */}
+				{/* Theme toggle button and sign-in/logout button*/}
 				<div className="flex items-center gap-4">
 					<ThemeToggle />
 					{session ? (
